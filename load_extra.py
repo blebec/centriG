c93de347b27eaa1cf72ea3dcd990b6d372221a92
chrisMacPortR <christophe.desbois@unic.cnrs-gif.fr>
2020-04-14 11:16:15 +0200

    load_extra : added the minimal function to be able to import the data in a pandas dataframe

--- load_extra.py
+++ load_extra.py
@@ -9,6 +9,8 @@ Created on Mon Apr 13 18:08:45 2020
 import os
 import pandas as pd
 from glob import glob
+import matplotlib.pyplot as plt
+
 
 cdir = '/Users/cdesbois/pg/chrisPg/centriG/dataExtra'
 os.chdir(cdir)
@@ -18,17 +20,25 @@ os.chdir(cdir)
 df = pd.DataFrame()
 #ser = pd.read_csv('Top_synch_150.txt')
 
-file_list = [y for x in os.walk(cdir) for y in glob(os.path.join(x[0], '*.txt'))]
 #%%
-df = pd.DataFrame()
-for file in file_list:
-    if os.path.basename(file)[0] not in ['r']:
-        #print(os.path.basename(file)[0])
-        ser = pd.read_csv(file, header=None)
-        name = os.path.basename(file).split('.')[0]
-        ser.columns = [name]
-        if len(ser) < 300:
-            df = df.join(ser, how='outer')
-            print (os.path.basename(file))
-            print(len(ser))
-            
+#NB look at the readme file in the dataExtra folder 
+
+def load_extra_data(path, bin_dur=5):
+    file_list = [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*.txt'))]
+    adf = pd.DataFrame()
+    for file in file_list:
+        if os.path.basename(file)[0] not in ['r']:
+            #print(os.path.basename(file)[0])
+            df = pd.read_csv(file, header=None)
+            name = os.path.basename(file).split('.')[0]
+            df.columns = [name]
+            if len(df) < 300:
+                adf = adf.join(df, how='outer')
+                # print (os.path.basename(file))
+                # print(len(df))
+    adf.index = adf.index * bin_dur
+    return adf
+
+df = load_extra_data(cdir)
+ser150 = pd.read_csv('Top_synch_150.txt')
+sert25 = pd.read_csv('Top_synch_25.txt')
