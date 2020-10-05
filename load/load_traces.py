c791e600ba2d6a45cfd01cb94e26cc2e8507455a
chrisMacPortR <christophe.desbois@cnrs.fr>
2020-10-05 14:49:26 +0200

    added an align keyword in the loading process and allowed the p2p key

--- load/load_traces.py
+++ load/load_traces.py
@@ -23,6 +23,23 @@ paths = config.build_paths()
 
 #%%
 
+def list_files(paths, age='new'):
+    """
+    list the file sin the index folders    
+    
+    """
+    file_list = ''
+    if age == 'old':
+        dirname = os.path.join(paths['owncFig'], 'data', 'old')
+        file_list = os.listdir(dirname)
+    elif age == 'new':
+        dir_name = os.path.join(paths['owncFig'],
+                                'data', 'averageTraces', 'controlsFig')
+        file_list = os.listdir(dir_name)
+    for file in file_list:
+        print(file)
+    return file_list
+
 def load_intra_mean_traces(paths, **kwargs):
 #def plot_figure3(stdcolors, *args, **kwargs):
     """
@@ -30,10 +47,11 @@ def load_intra_mean_traces(paths, **kwargs):
     old start of plot_figure3
     input :
         paths dico (from centriG.config.build_paths)
-        kind : in ['pop', 'sig', 'nsig'] (default = new)
-        rec : in ['vm', 'spk'] (default = vm)
-        spread : in ['sect', 'full'] (default = sect)
-        treat : in ['normAlign', 'raw', 'peak']
+        kwargs :
+            kind : in ['pop', 'sig', 'nsig'] (default = new)
+            rec : in ['vm', 'spk'] (default = vm)
+            spread : in ['sect', 'full'] (default = sect)
+            align : in ['normAlign', 'raw', 'peak', 'p2p']
     """
     kind = kwargs.get('kind', 'sig')
     # substract = kwargs.get('substract', False),
@@ -41,7 +59,7 @@ def load_intra_mean_traces(paths, **kwargs):
     age = kwargs.get('age', 'new')
     rec = kwargs.get('rec', 'vm')
     spread = kwargs.get('spread' , 'sect')
-    treat = kwargs.get('treat', 'normAlign')
+    align = kwargs.get('align', 'normAlign')
 
     file = ''
     file_list = ''
@@ -62,25 +80,34 @@ def load_intra_mean_traces(paths, **kwargs):
         kind = kind.lower()
         rec = rec.lower()
         spread = spread.lower()
-        treat = treat.lower()
+        align = align.lower()
         if kind in ['pop', 'sig', 'nsig']:
             file_list = [item for item in file_list if item.lower().startswith(kind)]
         else:
             print('kind should be in [pop, sig or nsig]')
-            return
+            return df, file
         file_list = [item for item in file_list if rec in item.lower()]
         file_list = [item for item in file_list if spread in item.lower()]
-        file_list = [item for item in file_list if treat in item.lower()]
-
-        file = file_list[0]
+        file_list = [item for item in file_list if align in item.lower()]
+        if len(file_list) < 1:
+            print('no file corresponding to the criteria')
+            print(kwargs)
+            return df, file
+        else:
+            print('founded')
+            for st in file_list:
+                print('\t {}'.format(st))
+            file = file_list[0]
+            print('selected \n \t {}'.format(file))
         filename = os.path.join(dir_name, file)
         df = pd.read_excel(filename)
     else:
         print('non defined')
 
-    #rename cols    
-    cols = df.columns.to_list()
-    cols = gfunc.new_columns_names(cols)
+    # rename cols
+    # name to snake case
+    cols = gfunc.new_columns_names(df.columns.to_list())
+    # sig in filename to sig in column name
     if file.startswith('sig'):
         cols = [item.replace('pop', 'sig') for item in cols]
     elif file.lower().startswith('nsig'):
@@ -92,7 +119,6 @@ def load_intra_mean_traces(paths, **kwargs):
 
 #%%
 if __name__ == '__main__':
-
     dico = dict(
         kind = 'pop',
         substract = False,
@@ -101,7 +127,11 @@ if __name__ == '__main__':
         rec='vm',
         spread='sect'
         )
-    dico['age'] = 'new'
+    dico['kind'] = 'sig'
+    dico['align'] = 'p2p'
     data_df, file = load_intra_mean_traces(paths, **dico)
-    print(f'loaded files : {file:}')
-    print('columns : ', data_df.columns.tolist())
+    if len(data_df) > 0:
+        print(f'loaded file :\n \t {file:}')
+        print('columns : ')
+        for item in data_df.columns.tolist():
+            print(f"\t {item}")
