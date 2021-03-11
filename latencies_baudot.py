6555cc3e0e95f9c08fe6ae6c96069e009f5126f0
chrisMacPortR <christophe.desbois@cnrs.fr>
2021-03-11 13:54:30 +0100

    latencies_baudot : a working version for vm -> spk onset tranfert function

--- latencies_baudot.py
+++ latencies_baudot.py
@@ -64,30 +64,94 @@ filename = '/Users/cdesbois/ownCloud/cgFigures/data/baudot/scatterData/scatLat.x
 # df = data_dict['DITRIB latence min max calcul 1']
 # df = data_dict['LATENCE PIERO S-C']
 #%%
-filename = '/Users/cdesbois/ownCloud/cgFigures/data/baudot/Figure latence GABY + histo.xls'
-data_dict = pd.read_excel(filename, None)
-for k in data_dict:
-    print(k)
-#
-df = data_dict['DITRIB latence min max calcul 1']
-# remove empty columns
-df = df.dropna(axis=1, how='all')
-df = df.drop(0)
 
-# kinds of stimulation formating
-df[df.columns[0]] = df[df.columns[0]].fillna(method='ffill')
-df[df.columns[0]] = df[df.columns[0]].apply(lambda st: '_'.join(st.lower().split(' ')))
-df[df.columns[-1]] = df[df.columns[-1]].fillna(method='ffill')
-df[df.columns[-1]] = df[df.columns[-1]].apply(lambda st: st.lower())
-df = df.rename(columns = {df.columns[0]: 'stim', df.columns[-1]:'repr'})
+def load_onsets():
+    filename = '/Users/cdesbois/ownCloud/cgFigures/data/baudot/Figure latence GABY + histo.xls'
+    data_dict = pd.read_excel(filename, None)
+    for k in data_dict:
+        print(k)
+    #
+    df = data_dict['DITRIB latence min max calcul 1']
+    # remove empty columns
+    df = df.dropna(axis=1, how='all')
+    df = df.drop(0)
 
-# manage columns names
-cols = [st.lower() for st in df.columns]
-cols = [_.replace('correlation', 'corr') for _ in cols]
-cols = [_.replace('airsign', 'sig') for _ in cols]
-cols = ['_'.join(_.split(' ')) for _ in cols]
-cols[-4] = cols[-4].replace('lat_sig_', '').split('.')[0]
-cols[-4] = cols[-4].replace('_s', '_seq').split('.')[0]
-cols[-3] = cols[-3].replace('lat_sig_', '').split('.')[0]
-cols[-3] = cols[-3].replace('_s', '_seq').split('.')[0]
-df.columns = cols
+    # kinds of stimulation formating
+    df[df.columns[0]] = df[df.columns[0]].fillna(method='ffill')
+    df[df.columns[0]] = df[df.columns[0]].apply(lambda st: '_'.join(st.lower().split(' ')[::-1]))
+    df[df.columns[-1]] = df[df.columns[-1]].fillna(method='ffill')
+    df[df.columns[-1]] = df[df.columns[-1]].apply(lambda st: st.lower())
+    df = df.rename(columns = {df.columns[0]: 'stim', df.columns[-1]:'repr'})
+
+    # manage columns names
+    cols = [st.lower() for st in df.columns]
+    cols = [_.replace('correlation', 'corr') for _ in cols]
+    cols = [_.replace('airsign', 'sig') for _ in cols]
+    cols = ['_'.join(_.split(' ')) for _ in cols]
+    cols[-4] = cols[-4].replace('lat_sig_', '').split('.')[0]
+    cols[-4] = cols[-4].replace('_s', '_seq').split('.')[0]
+    cols[-3] = cols[-3].replace('lat_sig_', '').split('.')[0]
+    cols[-3] = cols[-3].replace('_s', '_seq').split('.')[0]
+    df.columns = cols
+    return df
+
+
+data_df = load_onsets()
+#%%
+
+def plot_onsetTransfertFunc(df):
+    """
+    plot the vm -> time onset transfert function
+    """
+    values = ['moy_c-p', 'psth_seq-c']
+    stims = df.stim.unique()
+    markers = {'cf' : '^', 'cp' : 'v'}
+    colors = ['tab:brown', std_colors['green'],
+              std_colors['yellow'],std_colors['red']]
+
+    fig = plt.figure(figsize=(8, 6))
+    fig.suptitle('spk Vm onset-time transfert function')
+    ax = fig.add_subplot(111)
+
+    for i, stim in enumerate(stims):
+        temp = df.loc[df.stim == stim, values].dropna()
+        x = -1 * temp[values[0]].values
+        y = temp[values[1]].values
+        ax.scatter(x, y, color=colors[i], marker=markers[stim.split('_')[0]],
+                   s=100, alpha=0.8, label=stim, edgecolor='w')
+        # regress:
+        x = x.reshape(len(x), 1)
+        y = y.reshape(len(x), 1)
+        regr = linear_model.LinearRegression()
+        regr.fit(x,y)
+        ax.plot(x, regr.predict(x), color=colors[i], linestyle= ':',
+                linewidth=3, alpha=0.7)
+    ax.legend()
+    ax.axhline(0, color='tab:blue', linewidth=2, alpha=0.8)
+    ax.axvline(0, color='tab:blue', linewidth=2, alpha=0.8)
+    ax.set_ylabel('spikes onset relative latency (msec)')
+    ax.set_xlabel('Vm onset relative latency (msec)')
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+
+    ax.set_ylim(-30, 30)
+    ax.set_xlim(-30, 70)
+
+    if anot:
+        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        fig.text(0.99, 0.01, 'latencies_baudot.py:plot_onsetTransfertFunc',
+                 ha='right', va='bottom', alpha=0.4)
+        fig.text(0.01, 0.01, date, ha='left', va='bottom', alpha=0.4)
+    fig.tight_layout()
+    return fig
+
+
+plt.close('all')
+fig = plot_onsetTransfertFunc(data_df)
+
+save = False
+if save:
+    file = 'onsetTransfertFunc.png'
+    dirname = os.path.join(paths['owncFig'], 'pythonPreview', 'baudot')
+    filename = os.path.join(dirname, file)
+    fig.savefig(filename)
