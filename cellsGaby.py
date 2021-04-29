ca5d383563f88cedf4e40e0526912b7bc7982c87
chrisMacPortR <christophe.desbois@cnrs.fr>
2021-04-29 11:34:06 +0200

    cellsGaby -> adapted columns names

--- cellsGaby.py
+++ cellsGaby.py
@@ -14,6 +14,7 @@ import pandas as pd
 
 import config
 
+paths = config.build_paths()
 dirname = os.path.join(paths['owncFig'], 'data', 'baudot')
 files = [st for st in os.listdir(dirname) 
          if not os.path.isdir(os.path.join(dirname, st))]
@@ -23,6 +24,23 @@ for file in files:
 file = 'dataGaby2005.xls'
 filename = os.path.join(dirname, file)
 #%%
+df = pd.read_excel(filename, header=None)
 
-df = pd.read_excel(filename, header=2)
+# column names
 # nb df.head(3) -> header on three lines
+df.iloc[df.index[0:4]] = df.iloc[df.index[0:4]].astype('str')
+cols = df.iloc[df.index[0:4]].apply(lambda st: st.str.cat(sep=', '))
+cols = [_.replace('nan,', '') for _ in cols]
+cols = [_.strip() for _ in cols]
+cols = [_.replace('nan', '') for _ in cols]
+cols = [_.replace(',', '') for _ in cols]
+cols = [_.replace('  ', ' ') for _ in cols]
+cols = [_.strip() for _ in cols]
+cols = [_.replace('NOM', '') for _ in cols]
+cols = ['nd' if len(_)<1 else _ for _ in cols]
+cols = [_.strip() for _ in cols]
+cols = [_.lower() for _ in cols]
+df.columns = cols
+df = df.drop(index=range(4))
+
+# drop empty
