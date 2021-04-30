109beee8a9e2a74c00aacbb382ad36a060ae0ff2
chrisMacPortR <christophe.desbois@cnrs.fr>
2021-04-30 16:24:53 +0200

    a first plot to check variability

--- cellsGaby.py
+++ cellsGaby.py
@@ -7,8 +7,9 @@ Created on Wed Apr 28 13:53:38 2021
 """
 
 import os
+from datetime import datetime
 
-
+import matplotlib.pyplot as plt
 import pandas as pd
 
 import config
@@ -27,7 +28,7 @@ for file in files:
 
 file = 'dataGaby2005.xls'
 filename = os.path.join(dirname, file)
-#%%
+#
 def load_gaby():
     df = pd.read_excel(filename, header=None)
     # remove empty columns
@@ -156,7 +157,23 @@ def load_gaby():
     cols = [_.replace('_(na)', '(na)') for _ in cols]
     cols = [_.replace('_(microns)', '(microns)') for _ in cols]
     cols = [_.replace('solut.', 'solut') for _ in cols]
-
+    cols = [_.replace('_sel.', '_sel') for _ in cols]
+    cols = [_.replace('.opt.', 'Opt') for _ in cols]
+    cols = [_.replace('_(s)', '(s)') for _ in cols]
+    cols = [_.replace('_(min)', '(min)') for _ in cols]
+    cols = [_.replace('.(mv)', '(mv)') for _ in cols]
+    cols = [_.replace('%explor.', 'explor') for _ in cols]
+    cols = [_.replace('%exp', 'exp') for _ in cols]
+    cols = [_.replace('_vmcor.', '_vmcor') for _ in cols]
+    cols = [_.replace('_(vm)', '(vm)') for _ in cols]
+    cols = [_.replace('_kg', '(kg)') for _ in cols]
+    cols = [_.replace('_i.clamp', '_iclamp') for _ in cols]
+    cols = [_.replace('nom', 'cell') for _ in cols]
+    # to be chekced
+    cols = [_.replace('(m1)', '') for _ in cols]
+    
+    
+    
     df.columns = cols
     df = df.drop(index=range(5))
 
@@ -164,25 +181,155 @@ def load_gaby():
     df = df.drop(index=[193, 194, 201, 208])
 
     # fill names
-    df.nom = df.nom.fillna(method='ffill')
+    df.cell = df.cell.fillna(method='ffill')
 
     return df
 
 data_df = load_gaby()
 
+# for col in data_df.columns:
+#     if col.startswith('grat'):
+#         print(col)
+        
 #%%
 # NB positions = ['gaby_cr', 'visuel_ac_od', 'visuel_ac_og',
 # 'revcor_cr', 'barfl_cr', 'barmv_cr', 'grat_cr(m1)'
 
-select = ['nom', 'gaby_cr_x', 'gaby_cr_y', 'gaby_cr_l', 'gaby_cr_w', 'gaby_cr_theta'
-        'revcor_cr_x', 'revcor_cr_y', 'revcor_cr_l', 'revcor_cr_w', 'revcor_cr_theta']
+def table_loc_key():
+    keys = {_.split('_')[0] for _ in data_df.columns}
+    coord_loc = []
+    for key in keys:
+        select = [st for st in data_df.columns if key in st]
+        select = [st for st in select if '_cr' in st]
+        select = [st for st in select if st.endswith(('x', 'y', 'l', 'w', 'theta'))]
+        if len(select) > 0:
+            coord_loc.append(key)
+    print('keys for location are {}'.format(coord_loc))
+    return coord_loc
 
-select = [st for st in data_df.columns if 'revcor' in st]
-select = [st for st in select if '_cr' in st]
-select = [st for st in select if st.endswith(('x', 'y', 'l', 'w', 'theta'))]
-select.insert(0, 'nom')
+def select_corrd(key='revcor', printcells=False):
+    select = [st for st in data_df.columns if 'revcor' in st]
+    select = [st for st in select if '_cr' in st]
+    select = [st for st in select if st.endswith(('x', 'y', 'l', 'w', 'theta'))]
+    select.insert(0, 'cell')
+    df = data_df[select].copy()
+    df = df.loc[df.cell.notnull()]
+    cells = df.dropna(how='any')
+    print('for key={} we have {} cells ({} records)'.format(
+        key, len(cells.cell.unique()), len(df)))
+    if printcells:
+        print(cells)
+    return df, cells
 
-df = data_df[select].copy()
-cells = df.dropna(how='any')
-print('{} cells'.format(len(cells.nom.unique())))
-print(cells)
+keys = table_loc_key()
+df, cells = select_corrd()
+
+# for key in keys:
+#     df, _ = select_corrd(key)
+
+#%% look at all cordinates locations
+key = 'revcor'
+_, cells = select_corrd(key)
+
+
+# fig, ax = plt.subplots(figsize=(8,8))
+# ax.scatter(cells.revcor_cr_x, cells.cell)
+plt.close('all')
+
+def plot_xy_distri(key='revcor', bins=10):
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
+    axes = axes.flatten()
+    fig.suptitle(key)
+
+    # xy
+    ax = axes[0]
+    ax.set_title('(x y)')
+    for neur in cells.cell.unique():
+        x = cells.loc[cells.cell == neur, ['revcor_cr_x']].values
+        y = cells.loc[cells.cell == neur, ['revcor_cr_y']].values
+        ax.scatter(x,y, alpha=0.5)
+        ax.plot(x,y, alpha=0.5)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    ax = axes[1]
+    ax.set_title('length')
+    for neur in cells.cell.unique():
+        x = cells.loc[cells.cell == neur, ['revcor_cr_l']].values
+        ax.plot(x, marker='o', alpha=0.7)
+    ax.set_xlabel('num')
+    ax.set_ylabel('length')
+
+    ax = axes[2]
+    ax.set_title('length')
+    meds = []
+    for neur in cells.cell.unique():
+        med = cells.loc[cells.cell == neur, ['revcor_cr_l']].median()
+        meds.append(med)
+    y, x = np.histogram(meds, bins=bins)
+    width = round((np.max(meds) - np.min(meds))/bins * .9)
+    ax.bar(x=x[:-1], width=width, height=y, align='edge', alpha=0.6)
+    ax.set_xlabel('length')
+    ax.yaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    ax = axes[3]
+    ax.set_title('theta')
+    for neur in cells.cell.unique():
+        x = cells.loc[cells.cell == neur, ['revcor_cr_theta']].values
+        ax.plot(x, marker='o', alpha=0.7)
+    ax.set_xlabel('num')
+    ax.set_ylabel('theta')
+
+    ax = axes[4]
+    ax.set_title('width')
+    for neur in cells.cell.unique():
+        x = cells.loc[cells.cell == neur, ['revcor_cr_w']].values
+        ax.plot(x, marker='o', alpha=0.7)
+    ax.set_xlabel('num')
+    ax.set_ylabel('width')
+
+    ax = axes[5]
+    ax.set_title('width')
+    meds = []
+    for neur in cells.cell.unique():
+        med = cells.loc[cells.cell == neur, ['revcor_cr_w']].median()
+        meds.append(med)
+    y, x = np.histogram(meds, bins=bins)
+    width = round((np.max(meds) - np.min(meds))/bins * .9)
+    ax.bar(x=x[:-1], width=width, height=y, align='edge', alpha=0.6)
+    ax.set_xlabel('width')
+    ax.yaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+
+    for i, ax in enumerate(fig.get_axes()):
+        # ax.set_title(i)
+        for spine in ['top', 'right']:
+            ax.spines[spine].set_visible(False)
+    if anot:
+        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        fig.text(0.99, 0.01, 'cellsGaby:plot_xy_distri_' + key,
+                 ha='right', va='bottom', alpha=0.4)
+        fig.text(0.01, 0.01, date, ha='left', va='bottom', alpha=0.4)
+
+    fig.tight_layout()
+    
+    return fig
+    
+plt.close('all')
+anot=True
+fig = plot_xy_distri()    
+
+#%%
+# bug 
+
+y, x = np.histogram(meds)
+
+ax.bar(x=x[:-1], width = 0.8, height=y, align='edge')
+       width=(max(y)- min(y)) / len(y), height=x)
+
+cells.groupby('cell')['revcor_cr_l'].mean()
