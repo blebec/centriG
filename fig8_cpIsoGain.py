34051b6afc16051cea8e1d3e3aa0768056eae622
chrisMacPortR <christophe.desbois@cnrs.fr>
2021-11-25 15:09:11 +0100

    fig8_cpIsoGain : loading data

--- fig8_cpIsoGain.py
+++ fig8_cpIsoGain.py
@@ -42,12 +42,115 @@ std_colors = config.std_colors()
 speed_colors = config.speed_colors()
 plt.rcParams.update(config.rc_params())
 paths = config.build_paths()
+paths["sup"] = os.path.join(
+    paths["owncFig"], "pythonPreview", "current", "fig_data_sup"
+)
+
 os.chdir(paths["pg"])
 
+#%%
+# age = ["old", "new"][1]
+# if "fig2_df" not in globals():
+#     fig2_df, fig2_cols = ldat.load2(age)
 
-#%%%%%%%%%%%  copy pasta
-def get_sig3_df(key="sector"):
-    """ get sig amp U time U fill data """
+
+def load_fig8_cpIsoGain_initial(printTraces=False):
+    filename = os.path.join(paths["pg"], "data", "data_to_use", "fig2_2traces.xlsx")
+    inidf = pd.read_excel(filename, engine="openpyxl")
+    # centering
+    middle = (inidf.index.max() - inidf.index.min()) / 2
+    inidf.index = (inidf.index - middle) / 10
+    inidf = inidf.loc[-200:150]
+    cols = inidf.columns
+    cols = [_.casefold() for _ in cols]
+    cols = [_.replace("vm", "_vm_") for _ in cols]
+    cols = [_.replace("spk", "_spk_") for _ in cols]
+    cols = [_.replace("scpiso", "_s_cpiso_") for _ in cols]
+    cols = [_.replace("ctr", "_ctr_") for _ in cols]
+    cols = [_.replace("nsig", "_nsig_") for _ in cols]
+    cols = [_.replace("sig", "_sig_") for _ in cols]
+    cols = [_.replace("n_sig", "nsig") for _ in cols]
+    cols = [_.replace("_stc", "_stc_") for _ in cols]
+    cols = [_.replace("__", "_") for _ in cols]
+    cols = [_.strip("_") for _ in cols]
+    cols = [_.replace("_s_", "_") for _ in cols]
+    cols = [_.replace("pop_", "popN2sig_") if "_nsig" in _ else _ for _ in cols]
+    cols = [_.replace("_nsig", "") for _ in cols]
+    cols = [_.replace("pop_", "pop2sig_") if "_sig" in _ else _ for _ in cols]
+    cols = [_.replace("_sig", "") for _ in cols]
+    inidf.columns = cols
+
+    print("fig8_cpIsoGain_initial")
+    if printTraces:
+        for col in inidf.columns:
+            print(col)
+        print()
+
+    aset = set()
+    for _ in cols:
+        l = _.split("_")
+        for w in l:
+            aset.add(w)
+    print("keys are : {}".format(aset))
+    return inidf
+
+
+def load_fig8_cpIsoGain_sup(printTraces=False):
+    supfilename = os.path.join(paths["sup"], "fig8_supdata.xlsx")
+    supdf = pd.read_excel(supfilename, keep_default_na=True, na_values="")
+    # centering
+    middle = (supdf.index.max() - supdf.index.min()) / 2
+    supdf.index = (supdf.index - middle) / 10
+    supdf = supdf.loc[-200:150]
+    scols = supdf.columns
+    scols = [_.casefold() for _ in scols]
+    scols = [_.replace("ind_cell_", "indi_") for _ in scols]
+    scols = [_.replace("indi_", "indivm_") if "_vm_" in _ else _ for _ in scols]
+    scols = [_.replace("indi_", "indispk_") if "_spk_" in _ else _ for _ in scols]
+
+    scols = [_.replace("pop_37_", "pop_") for _ in scols]
+    scols = [_.replace("pop_22_", "pop_") for _ in scols]
+    scols = [_.replace("pop_15_", "pop2sig_") for _ in scols]
+    scols = [_.replace("pop_6_", "pop2sig_") for _ in scols]
+    scols = [_.replace("pop_20_", "pop3sig_") for _ in scols]
+    scols = [_.replace("pop_10_", "pop3sig_") for _ in scols]
+
+    for pop in ["pop", "pop2sig", "pop3sig"]:
+        scols = [_.replace(pop + "_", pop + "vm_") if "_vm" in _ else _ for _ in scols]
+        scols = [
+            _.replace(pop + "_", pop + "spk_") if "_spk" in _ else _ for _ in scols
+        ]
+
+    scols = [_.replace("_vm", "") for _ in scols]
+    scols = [_.replace("_spk", "") for _ in scols]
+    scols = [_.replace("vm_", "_vm_") for _ in scols]
+    scols = [_.replace("spk_", "_spk_") for _ in scols]
+
+    scols = [_.replace("ctr_stc_", "ctr_") for _ in scols]
+    scols = [_.replace("cpcross", "cpx") for _ in scols]
+
+    supdf.columns = scols
+
+    print("beware : an unamed 38 column exists")
+
+    print("fig8_cpIsoGain_sup")
+    if printTraces:
+        for col in supdf.columns:
+            print(col)
+        print()
+
+    aset = set()
+    for _ in scols:
+        l = _.split("_")
+        for w in l:
+            aset.add(w)
+    print("keys are : {}".format(aset))
+
+    return supdf
+
+
+def load_fig8_cpIsoGain_pop3sig(key="sector", printTraces=False):
+    """ get sig amp U time U fill data aka sig3"""
 
     if key != "sector":
         print("{} should be implemented".format(key))
@@ -58,19 +161,52 @@ def get_sig3_df(key="sector"):
     )
     sig3df = pd.read_excel(filename, engine="openpyxl")
     cols = gfunc.new_columns_names(sig3df.columns)
-    cols = [item.replace("sig_", "") for item in cols]
-    cols = [item.replace("_stc", "") for item in cols]
-    cols = [st.replace("_iso", "") for st in cols]
+    cols = [item.replace("sig_", "pop3sig_") for item in cols]
+    cols = [item.replace("full_rd", "frnd") for item in cols]
+    cols = [item.replace("sect_rd", "srnd") for item in cols]
+    cols = [st.replace("cp_iso", "cpiso") for st in cols]
+    cols = [st.replace("cf_iso", "cfiso") for st in cols]
+    cols = [st.replace("sect_cx", "cpx") for st in cols]
+    cols = [st.replace("_sect_", "_") for st in cols]
     cols = [st.replace("__", "_") for st in cols]
     cols = [st.replace("_.1", "") for st in cols]
     sig3df.columns = cols
     # adjust time scale
     middle = (sig3df.index.max() - sig3df.index.min()) / 2
     sig3df.index = (sig3df.index - middle) / 10
+
+    print("fig8_cpIsoGain_pop3sig")
+    if printTraces:
+        for col in sig3df.columns:
+            print(col)
+        print()
+
+    aset = set()
+    for _ in cols:
+        l = _.split("_")
+        for w in l:
+            aset.add(w)
+    print("keys are : {}".format(aset))
+
     return sig3df
 
 
-sig3_df = get_sig3_df()
+ini_df = load_fig8_cpIsoGain_initial()
+sig3_df = load_fig8_cpIsoGain_pop3sig()
+sup_df = load_fig8_cpIsoGain_sup()
+
+cols = ini_df.columns
+inicols = ini_df.columns
+[_ for _ in inicols if _.startswith('indi')]
+df1 = ini_df[_ for _ in inicols if _.startswith('indi')]
+df1 = ini_df[[_ for _ in inicols if _.startswith('indi')]]
+sigcols = sig3_df.colums
+sigcols = sig3_df.columns
+[_ for _ in sigcols if _.startswith('indi')]
+supcols = sup_df.columns
+[_ for _ in supcols if _.startswith('indi')]
+df2 = sup_df[[_ for _ in supcols if _.startswith('indi')]]
+
 
 
 #%%
